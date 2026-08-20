79835b2994de3f628dc71d281c61faa332705f25
zuohuadong <zuohuadong@gmail.com>
2026-08-20 18:12:21 +0800

    build: add Windows x64 electron-builder packaging pipeline and dist:desktop script

--- apps/desktop-electron/dist/preload.js
+++ apps/desktop-electron/dist/preload.js
@@ -1,18 +1,21 @@
-import { contextBridge, ipcRenderer } from 'electron';
-contextBridge.exposeInMainWorld('electronDsh', {
-    getServerUrl: () => ipcRenderer.invoke('dsh:get-server-url'),
-    openFolderDialog: () => ipcRenderer.invoke('dsh:open-folder-dialog'),
-    getWorkingDir: () => ipcRenderer.invoke('dsh:get-working-dir'),
-    setWorkingDir: (dir) => ipcRenderer.invoke('dsh:set-working-dir', dir),
-    getConfig: () => ipcRenderer.invoke('dsh:get-config'),
-    saveConfig: (cfg) => ipcRenderer.invoke('dsh:save-config', cfg),
-    minimizeWindow: () => ipcRenderer.invoke('dsh:window-minimize'),
-    maximizeWindow: () => ipcRenderer.invoke('dsh:window-maximize'),
-    closeWindow: () => ipcRenderer.invoke('dsh:window-close'),
-    isMaximized: () => ipcRenderer.invoke('dsh:window-is-maximized'),
-    toggleDevTools: () => ipcRenderer.invoke('dsh:toggle-devtools'),
-    onWindowStateChange: (callback) => {
-        ipcRenderer.on('dsh:window-state-changed', (_e, state) => callback(state));
-    },
+"use strict";
+
+// src/preload.ts
+var import_electron = require("electron");
+import_electron.contextBridge.exposeInMainWorld("electronDsh", {
+  getServerUrl: () => import_electron.ipcRenderer.invoke("dsh:get-server-url"),
+  openFolderDialog: () => import_electron.ipcRenderer.invoke("dsh:open-folder-dialog"),
+  getWorkingDir: () => import_electron.ipcRenderer.invoke("dsh:get-working-dir"),
+  setWorkingDir: (dir) => import_electron.ipcRenderer.invoke("dsh:set-working-dir", dir),
+  getConfig: () => import_electron.ipcRenderer.invoke("dsh:get-config"),
+  saveConfig: (cfg) => import_electron.ipcRenderer.invoke("dsh:save-config", cfg),
+  minimizeWindow: () => import_electron.ipcRenderer.invoke("dsh:window-minimize"),
+  maximizeWindow: () => import_electron.ipcRenderer.invoke("dsh:window-maximize"),
+  closeWindow: () => import_electron.ipcRenderer.invoke("dsh:window-close"),
+  isMaximized: () => import_electron.ipcRenderer.invoke("dsh:window-is-maximized"),
+  toggleDevTools: () => import_electron.ipcRenderer.invoke("dsh:toggle-devtools"),
+  onWindowStateChange: (callback) => {
+    import_electron.ipcRenderer.on("dsh:window-state-changed", (_e, state) => callback(state));
+  }
 });
 //# sourceMappingURL=preload.js.map
